fcc494b978f984726b0b0cc3c816e3a58662e663
David LaPorte <david@davidlaporte.org>
2025-09-01 10:21:14 -0400

    Update smartoilgauge.groovy

--- SmartOilGauge/app/smartoilgauge.groovy
+++ SmartOilGauge/app/smartoilgauge.groovy
@@ -12,6 +12,8 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ * For monitoring your oil tank using Smart Oil Gauge (https://www.smartoilgauge.com)
+ * You must subscribe to the Droplet Fuel 69 monitoring service ($2/month) for the required API access.
  */
 
 import groovy.json.*
